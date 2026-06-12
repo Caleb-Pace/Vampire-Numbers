5aa8a82a0fb8c21995ff9c0daf38c1a453f23845
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-06-12 19:18:58 +1200

    wip: add first two conditions for vampire numbers

--- src/vampire.cpp
+++ src/vampire.cpp
@@ -1,7 +1,33 @@
+#include <cmath>
+#include <vector>
 
 namespace VampireNums {
 
+    int count_digits(long number) {
+        return log10(number) + 1;
+    }
+
+    bool is_odd(int num) {
+        return num & 1;
+    }
+
+    // // TODO: Implement
+    // std::vector<char> get_digits(long number) {
+    // }
+
+    // Cannot be negative
+    // Even digits
+    // Has fangs
+    //   Can be factored
+    //   Fangs don't both end in zero
+    //   Equal length fangs
+    //   Digits in fangs match digits in number
     bool is_vampire_number(long number) {
+        if (number < 0)
+            return false;  // Cannot be negative
+        if (is_odd(count_digits(number)))
+            return false;  // Cannot have odd number of digits
+        
         return true;
     }
     
